665caeb7dc62783011b4b8f222e4fb38627ff717
Ben Hadfield <benjohnhadfield@gmail.com>
2018-04-10 11:27:55 +0100

    test tweaks

--- src/controllers/tracks/spec.js
+++ src/controllers/tracks/spec.js
@@ -1,3 +1,4 @@
+const nock = require('nock')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const expect = chai.expect
@@ -11,6 +12,28 @@ describe('tracks endpoint', () => {
         .get('/v1/search')
         .end((err, res) => {
           expect(res).to.have.status(400)
+          done()
+        })
+    })
+
+    it('should return JSON when successful', (done) => {
+      // response object
+      const data = {
+        items: {
+          href: 'foo',
+          items: []
+        }
+      }
+      // mock spotify
+      const spotify = nock('https://api.spotify.com')
+        .get('/v1/search')
+        .reply(200, { data })
+      
+      chai.request(server)
+        .get('/v1/search')
+        .query({ q: 'some-query' })
+        .end((err, res) => {
+          expect(res).to.be.json
           done()
         })
     })
